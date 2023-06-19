4188396b723e17ac250b74004b9012a8d65fcd2e
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-06-19 14:32:35 +0200

    header i lombok

--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -3,14 +3,17 @@ package com.songify;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@Log4j2
 public class SongsController {
 
     Map<Integer, String> database = new HashMap<>();
@@ -34,7 +37,8 @@ public class SongsController {
     }
 
     @GetMapping("/songs/{id}")
-    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id) {
+    public ResponseEntity<SingleSongResponseDto> getSongById(@PathVariable Integer id, @RequestHeader(required = false) String requestId) {
+        log.info(requestId);
         String song = database.get(id);
         if (song == null) {
             return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
