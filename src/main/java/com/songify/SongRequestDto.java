73639eed749488dbed4701187a72c1f0dcaadc2e
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-06-21 12:05:26 +0200

    400 bad request when not valid

--- src/main/java/com/songify/SongRequestDto.java
+++ src/main/java/com/songify/SongRequestDto.java
@@ -1,4 +1,11 @@
 package com.songify;
 
-public record SongRequestDto(String songName) {
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+public record SongRequestDto(
+        @NotNull(message = "songName must not be null")
+        @NotEmpty(message = "songName must not be empty")
+        String songName
+) {
 }
--- src/main/java/com/songify/SongsController.java
+++ src/main/java/com/songify/SongsController.java
@@ -1,5 +1,6 @@
 package com.songify;
 
+import jakarta.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.stream.Collectors;
@@ -51,7 +52,7 @@ public class SongsController {
     }
 
     @PostMapping("/songs")
-    public ResponseEntity<SingleSongResponseDto> postSong(@RequestBody SongRequestDto request) {
+    public ResponseEntity<SingleSongResponseDto> postSong(@RequestBody @Valid SongRequestDto request) {
         String songName = request.songName();
         log.info("adding new song: " + songName);
         database.put(database.size() + 1, songName);
