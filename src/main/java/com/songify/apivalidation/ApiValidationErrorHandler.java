50bdcbe8a311e459c0180a008db6c07841ee2d5f
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-07-06 14:44:42 +0200

    api error handling refactor

--- src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
+++ src/main/java/com/songify/apivalidation/ApiValidationErrorHandler.java
@@ -1,25 +1,22 @@
 package com.songify.apivalidation;
 
-import com.songify.song.infrastructure.controller.SongRestController;
 import java.util.List;
 import java.util.stream.Collectors;
 import org.springframework.context.support.DefaultMessageSourceResolvable;
-import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
 
-@ControllerAdvice(assignableTypes = SongRestController.class)
+@ControllerAdvice
 public class ApiValidationErrorHandler {
 
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    @ResponseBody
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiValidationErrorResponseDto handleValidationException(MethodArgumentNotValidException exception) {
+    public ResponseEntity<ApiValidationErrorResponseDto> handleValidationException(MethodArgumentNotValidException exception) {
         List<String> messages = getErrorsFromException(exception);
-        return new ApiValidationErrorResponseDto(messages, HttpStatus.BAD_REQUEST);
+        ApiValidationErrorResponseDto response = new ApiValidationErrorResponseDto(messages, BAD_REQUEST);
+        return ResponseEntity.status(BAD_REQUEST).body(response);
     }
 
     private List<String> getErrorsFromException(MethodArgumentNotValidException exception) {
