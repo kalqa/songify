1e5c39fd4e4cbaf0615458c281c1d5796f89067b
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-07-11 12:52:56 +0200

    schema

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,8 +1,8 @@
--- DROP TABLE song;
---
--- CREATE TABLE IF NOT EXISTS song
--- (
---     id     BIGSERIAL    PRIMARY KEY,
---     artist VARCHAR(255) NOT NULL,
---     name   VARCHAR(255) NOT NULL
--- );
+DROP TABLE song;
+
+CREATE TABLE IF NOT EXISTS song
+(
+    id     BIGSERIAL    PRIMARY KEY,
+    artist VARCHAR(255) NOT NULL,
+    name   VARCHAR(255) NOT NULL
+);
