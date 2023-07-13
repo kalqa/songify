49c00e5336faad7e9113322c3a27fc0e33bd1d81
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-07-13 14:12:47 +0200

    dirty checking version added

--- src/main/java/com/songify/song/domain/service/SongUpdater.java
+++ src/main/java/com/songify/song/domain/service/SongUpdater.java
@@ -41,4 +41,22 @@ public class SongUpdater {
         updateById(id, toSave);
         return toSave;
     }
+
+// Dirty checking version
+//    public void updateById(Long id, Song newSong) {
+//        Song songById = songRetriever.findSongById(id);
+//        songById.setName(newSong.getName());
+//        songById.setArtist(newSong.getArtist());
+//    }
+//
+//    public Song updatePartiallyById(Long id, Song songFromRequest) {
+//        Song songFromDatabase = songRetriever.findSongById(id);
+//        if (songFromRequest.getName() != null) {
+//            songFromDatabase.setName(songFromRequest.getName());
+//        }
+//        if (songFromRequest.getArtist() != null) {
+//            songFromDatabase.setArtist(songFromRequest.getArtist());
+//        }
+//        return songFromDatabase;
+//    }
 }
