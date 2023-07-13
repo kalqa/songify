d411dfa44d9aabcafabd129f274a1cbc44bae2e4
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-07-13 14:18:27 +0200

    query for rest of methods in spring data

--- src/main/java/com/songify/song/domain/repository/SongRepository.java
+++ src/main/java/com/songify/song/domain/repository/SongRepository.java
@@ -10,17 +10,21 @@ import java.util.Optional;
 
 public interface SongRepository extends Repository<Song, Long> {
 
-    Song save(Song song);
-
+    @Query("SELECT s FROM Song s")
     List<Song> findAll();
 
+    @Query("SELECT s FROM Song s WHERE s.id =:id")
     Optional<Song> findById(Long id);
 
+    @Modifying
+    @Query("DELETE FROM Song s WHERE s.id = :id")
     void deleteById(Long id);
 
     @Modifying
     @Query("UPDATE Song s SET s.name = :#{#newSong.name}, s.artist = :#{#newSong.artist} WHERE s.id = :id")
     void updateById(Long id, Song newSong);
+
+    Song save(Song song);
 
     boolean existsById(Long id);
 }
