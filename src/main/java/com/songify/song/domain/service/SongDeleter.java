d949d6173ec9dba7d85ba3a76c505d3b0e5a6396
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2023-07-14 11:17:15 +0200

    transactional to rest

--- src/main/java/com/songify/song/domain/service/SongDeleter.java
+++ src/main/java/com/songify/song/domain/service/SongDeleter.java
@@ -1,11 +1,13 @@
 package com.songify.song.domain.service;
 
 import com.songify.song.domain.repository.SongRepository;
+import jakarta.transaction.Transactional;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 @Service
 @Log4j2
+@Transactional
 public class SongDeleter {
 
     private final SongRepository songRepository;
