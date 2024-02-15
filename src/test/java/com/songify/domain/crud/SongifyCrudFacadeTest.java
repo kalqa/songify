4ccea5c0013ad6c3dbcac85ae7a9a30518f1a522
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2024-02-15 15:11:53 +0100

    secondo

--- src/test/java/com/songify/domain/crud/SongifyCrudFacadeTest.java
+++ src/test/java/com/songify/domain/crud/SongifyCrudFacadeTest.java
@@ -2,6 +2,7 @@ package com.songify.domain.crud;
 
 import com.songify.domain.crud.dto.ArtistDto;
 import com.songify.domain.crud.dto.ArtistRequestDto;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -17,23 +18,29 @@ class SongifyCrudFacadeTest {
     );
 
     @Test
-    public void first() {
+    @DisplayName("Should add artist 'amigo' with id:0 When amigo was sent")
+    public void should_add_artist_amigo_with_id_zero_when_amigo_was_sent() {
+        // given
         ArtistRequestDto shawnMendes = ArtistRequestDto.builder()
                 .name("amigo")
                 .build();
+        // when
         ArtistDto response = songifyCrudFacade.addArtist(shawnMendes);
-
+        // then
         assertThat(response.id()).isEqualTo(0L);
         assertThat(response.name()).isEqualTo("amigo");
     }
 
     @Test
-    public void first_wariant_B() {
+    @DisplayName("Should add artist 'shawn mendes' with id:0 When shawn mendes was sent")
+    public void should_add_artist_shawn_mendes_with_id_zero_when_shawn_mendes_was_sent() {
+        // given
         ArtistRequestDto shawnMendes = ArtistRequestDto.builder()
                 .name("shawn mendes")
                 .build();
+        // when
         ArtistDto response = songifyCrudFacade.addArtist(shawnMendes);
-
+        // then
         assertThat(response.id()).isEqualTo(0L);
         assertThat(response.name()).isEqualTo("shawn mendes");
     }
