630f17302dc6c1b0d8fd9aa51fcdabfc8bd5c266
bartlomiejkalka <bartlomiejkalqa@gmail.com>
2024-04-04 14:43:20 +0200

    testcontainers and spring data connection to postgresql created by testcontainers, also overriding properties, also set postgresql version to fixed 15 prod/tests

--- src/integration/java/feature/HappyPathIntegrationTest.java
+++ src/integration/java/feature/HappyPathIntegrationTest.java
@@ -3,9 +3,25 @@ package feature;
 import com.songify.SongifyApplication;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest(classes = SongifyApplication.class)
+@Testcontainers
+@ActiveProfiles("integration")
 class HappyPathIntegrationTest {
+
+    @Container
+    static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>("postgres:15-alpine");
+
+    @DynamicPropertySource
+    public static void propertyOverride(DynamicPropertyRegistry registry) {
+        registry.add("spring.datasource.url", postgreSQLContainer::getJdbcUrl);
+    }
 
     @Test
     public void f(){
